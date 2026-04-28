40bb286196fdf21cb082a57ab14f87b4fead6ee3
Subir Datta <aiondatta1234@gmail.com>
2026-04-28 18:04:47 +0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'gold-calc-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
